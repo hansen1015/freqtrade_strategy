db0df8de59dd4b93702a5f277c36a8a890bde6b6
hansen khornelius <hansen.khornelius@gmail.com>
2021-04-20 18:34:31 +0800

    Add files via upload
    Heikin Ashi implementation on freqtrade. The heikin ashi open should be the previous average of close and open, however, I made it into the previous 2 open and close since it generate better profit and cleaner data.

---
+++ heikin.py
@@ -0,0 +1,55 @@
+# --- Do not remove these libs --- freqtrade backtesting --strategy SmoothScalp --timerange 20210110-20210410
+from freqtrade.strategy.interface import IStrategy
+from typing import Dict, List
+from functools import reduce
+from pandas import DataFrame
+# --------------------------------
+import talib.abstract as ta
+import freqtrade.vendor.qtpylib.indicators as qtpylib
+from typing import Dict, List
+from functools import reduce
+from pandas import DataFrame, DatetimeIndex, merge
+# --------------------------------
+import talib.abstract as ta
+import freqtrade.vendor.qtpylib.indicators as qtpylib
+import numpy  # noqa
+
+
+class heikin(IStrategy):
+    """
+        this strategy is based around the idea of generating a lot of potentatils buys and make tiny profits on each trade
+
+        we recommend to have at least 60 parallel trades at any time to cover non avoidable losses
+    """
+
+    timeframe = '1h'
+    minimal_roi = {
+        "0": 10,
+    }
+    stoploss = -0.99
+    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:   
+        dataframe['hclose']=(dataframe['open'] + dataframe['high'] + dataframe['low'] + dataframe['close']) / 4
+        dataframe['hopen']= ((dataframe['open'].shift(2) + dataframe['close'].shift(2))/ 2)
+        dataframe['hhigh']=dataframe[['open','close','high']].max(axis=1)
+        dataframe['hlow']=dataframe[['open','close','low']].min(axis=1)
+
+        dataframe['emac'] = ta.SMA(dataframe['hclose'], timeperiod=6)
+        dataframe['emao'] = ta.SMA(dataframe['hopen'], timeperiod=6)
+        return dataframe
+        
+
+    def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe.loc[
+            (
+                (dataframe['emao'] < dataframe['emac'])
+            ),
+            'buy'] = 1
+        return dataframe
+
+    def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe.loc[
+            (
+                (dataframe['emao'] > dataframe['emac'])
+            ),
+            'sell'] = 1
+        return dataframe
